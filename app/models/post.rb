fd730b9d58a3860c8a746860870371ca63888c05
daulen <daulenz@gmail.com>
2021-06-09 19:38:53 -0500

    commit tree b9af364aca9a0b260c901fdc8cc2aa0121089b37

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,23 @@
+require 'pry'
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+    
+
+    validate :clickbait?
+   PATTERNS = [/You Won't Believe These True Facts/i]
+
+    def clickbait?
+        # binding.pry
+       if  PATTERNS.none?{|pattern|pattern.match title}
+    
+        errors.add(:title, "Invalid!")
+      end
+    end
+
+
+
+
 end
